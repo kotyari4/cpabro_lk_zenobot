020c0998d7aa95831cc436d4fe13125ceb5cd9b1
EvgenyFedorov <rsa-team@yandex.ru>
2020-03-17 12:10:53 +0000

    Refresh and update packages vendor and edit migration failed job

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-17 11:51:32.
+    * Generated on 2020-03-17 12:08:01.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
